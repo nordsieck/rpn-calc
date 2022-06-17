83dddf725f494f39f8e92bd7c021d48a20f36b79
Theodore Nordsieck <theodore.nordsieck@gmail.com>
2022-06-17 07:40:28 -0400

    core calculator function

--- rpnc.py
+++ rpnc.py
@@ -24,6 +24,28 @@ class Calc:
 
         return data
 
+    def execute(self, value):
+        if type(value) is int:
+            self.the_stack.append(value)
+            return value
+
+        if len(self.the_stack) < value.operands:
+            return ERR_OPS
+
+        params = []
+        for i in range(value.operands):
+            params.append(self.the_stack.pop())
+
+        result = value.fn(*params)
+        self.the_stack.append(result)
+        return result
+
+    def process(self, data):
+        value = self.parse(data)
+        if value == ERR_INPUT:
+            return ERR_INPUT
+        return self.execute(value)
+
 class Error:
     def __init__(self, value):
         self.value = value
@@ -32,6 +54,7 @@ class Error:
         return self.value
 
 ERR_INPUT = Error("Error: Invalid input")
+ERR_OPS   = Error("Error: Insufficient operands")
 
 class Operator:
     def __init__(self, text, operands, fn):
@@ -68,3 +91,28 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(c.parse("5"), 5)
         self.assertNotEqual(c.parse("6"), 5)
         self.assertEqual(c.parse("w"), ERR_INPUT)
+
+    def testExecute(self):
+        c = Calc()
+
+        self.assertEqual(c.the_stack, [])
+        self.assertEqual(c.execute(ADD), ERR_OPS)
+
+        # operators
+        cases = [[ADD, 6],
+                 [SUB, 2],
+                 [MUL, 8],
+                 [DIV, 2]]
+
+        for i in range(len(cases)):
+            c.execute(2)
+            c.execute(4)
+            self.assertEqual(c.execute(cases[i][0]), cases[i][1])
+
+        # chain
+        c = Calc()
+        c.execute(2)
+        c.execute(4)
+        self.assertEqual(c.execute(ADD), 6)
+        c.execute(10)
+        self.assertEqual(c.execute(ADD), 16)
