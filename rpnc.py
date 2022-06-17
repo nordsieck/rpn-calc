d28a1faf6f6209e9ca1ffeb784d23a5c359da362
Theodore Nordsieck <theodore.nordsieck@gmail.com>
2022-06-17 07:37:02 -0400

    parse operators

--- rpnc.py
+++ rpnc.py
@@ -7,6 +7,16 @@ class Calc:
 
     def parse(self, data):
 
+        # I currently have python 3.8 installed, would use "match" here with 3.10
+        if data == "+":
+            return ADD
+        if data == "-":
+            return SUB
+        if data == "*":
+            return MUL
+        if data == "/":
+            return DIV
+
         try:
             data = int(data)
         except ValueError:
@@ -23,9 +33,36 @@ class Error:
 
 ERR_INPUT = Error("Error: Invalid input")
 
+class Operator:
+    def __init__(self, text, operands, fn):
+        self.text = text
+        self.operands = operands
+        self.fn = fn
+
+ADD = Operator("+", 2, lambda a, b: a + b)
+SUB = Operator("-", 2, lambda a, b: a - b)
+MUL = Operator("*", 2, lambda a, b: a * b)
+DIV = Operator("/", 2, lambda a, b: a / b)
+
 class TestCalc(unittest.TestCase):
     def testParse(self):
         c = Calc()
+
+        # +
+        val = c.parse("+")
+        self.assertEqual(val.fn(4, 2), 6)
+
+        # -
+        val = c.parse("-")
+        self.assertEqual(val.fn(4, 2), 2)
+
+        # *
+        val = c.parse("*")
+        self.assertEqual(val.fn(4, 2), 8)
+
+        # /
+        val = c.parse("/")
+        self.assertEqual(val.fn(4, 2), 2)
 
         # int
         self.assertEqual(c.parse("5"), 5)
